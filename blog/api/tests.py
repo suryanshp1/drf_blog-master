f330e0dd8f440d28f64042d1da3a64c71b1121f7
suryanshp1 <suryanshp1@gmail.com>
2021-12-15 14:55:26 +0530

    APITestCase Issue Resolved

--- blog/api/tests.py
+++ blog/api/tests.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from .models import Post
+import json
 
 class PostUrlsTests(APITestCase):
     def test_get_post_is_resolved(self):
@@ -14,60 +16,63 @@ class PostUrlsTests(APITestCase):
         url = reverse('posts')
         self.assertEqual(resolve(url).func.view_class, PostList)
 
+class RegistrationTestCase(APITestCase):
+    register = reverse('auth_register')
+    def test_registration(self):
+        data = {'username': 'testcase', 'first_name': 'Test', 'last_name': 'Man', 'email': 'test@email.com', 'password': 'some_strong_password'}
+        response = self.client.post(self.register, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 class GlobalSetup(APITestCase):
     post_urls = reverse('posts')
-    post_url = reverse('posts-detail', args=[5])
+    post_url = reverse('posts-detail', args=[1])
 
     def setUp(self):
-        self.user = User.objects.create_user(username='admin', password='admin@123')
+        self.user = User.objects.create_user(username='admin', password='admin@123', is_staff=True)
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token '+ self.token.key)
 
-        # Saving User
-        data = {
-            "title": 'Decentralization',
-            "body": 'Peer to Peer network is an example of decentralization'
-        }
-        response = self.client.post(self.post_url, data, format='json')
+        # Saving post
+
+        post_data = Post.objects.create(title="TestTitle", body="Peer to Peer network", owner=self.user)
+        
+        post_data.save()
+
+        self.post_data = post_data
 
 class PostAPIViewTests(GlobalSetup):
-
+    user_detail = reverse('users-detail', args=[1])
     def test_get_posts_authenticated(self):
         response = self.client.get(self.post_urls)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_posts_un_authenticated(self):
-        self.client.force_authenticate(user=None, token=None)
+        self.client.force_authenticate(user=self.user, token=self.token)
         response = self.client.get(self.post_urls)
         self.assertEqual(response.status_code, 200)
+
+    def test_user_detail(self):
+        response = self.client.get(self.user_detail)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['username'], 'admin')
     
     def test_post_posts_authenticated(self):
         self.client.force_authenticate(user=self.user, token=self.token)
         data = {
-            "title": 'Decentralization',
-            "body": 'Peer to Peer network is an example of decentralization'
+            "title": "Decentralization",
+            "body": "Peer to Peer network is an example of decentralization"
         }
         response = self.client.post(self.post_urls, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['title'], 'Decentralization')
 
-class PostDetailAPIView(GlobalSetup):
-
-    post_urls = reverse('posts')
-    post_url = reverse('posts-detail', args=[5])
-
-    def test_get_post_authenticated(self):
-        self.client.force_authenticate(user=self.user, token=self.token)
-        response = self.client.get(self.post_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['title'], 'Decentralization')
-
-    def test_get_post_un_authenticated(self):
-        self.client.force_authenticate(user=None, token=None)
-        response = self.client.get(self.post_urls)
-        self.assertEqual(response.status_code, 200)
-
     def test_delete_post_authenticated(self):
         self.client.force_authenticate(user=self.user, token=self.token)
-        response = self.client.delete(self.post_url)
+        response = self.client.delete(self.post_url, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_post_authenticated(self):
+        self.client.force_authenticate(user=self.user, token=self.token)
+        response = self.client.put(self.post_url, {"title": "TestTitle", "body": "Peer to Peer network"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), {"id": 1, "title": "TestTitle", "body": "Peer to Peer network", "owner": "admin", "comments": []})
